c305bbf5f2b46b95fbb61dac60e55b1264dba14e
abdullah <abdullah.2010bd@gmail.com>
2019-12-14 00:25:59 +0600

    Re-selecting feature for better accuracy

--- Abu_Hasnat_Abdullah_1510853042/main_file.py
+++ Abu_Hasnat_Abdullah_1510853042/main_file.py
@@ -4,6 +4,36 @@ import seaborn as sns
 from sklearn.ensemble import ExtraTreesClassifier
 import matplotlib.pyplot as plt
 from matplotlib import pyplot as plt
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import svm
+from sklearn.model_selection import cross_val_score
 
-df = pd.read_csv("Data/globalterrorismdb_0718dist.csv", encoding = "ISO-8859-1", low_memory = False)
-df.head()
+print("HEllo")
+data = pd.read_csv("globalterrorismdb_0718dist.csv", encoding = "ISO-8859-1", low_memory = False)
+X = data.iloc[:,0:133]  #independent columns
+y = data.iloc[:,-1]    #target column i.e price range
+
+bestfeatures = SelectKBest(score_func=chi2, k=25)
+
+#apply SelectKBest class to extract top 25 best features
+bestfeatures = SelectKBest(score_func=chi2, k=25)
+fit = bestfeatures.fit(X,y)
+dfscores = pd.DataFrame(fit.scores_)
+dfcolumns = pd.DataFrame(X.columns)
+#concat two dataframes for better visualization
+featureScores = pd.concat([dfcolumns,dfscores],axis=1)
+featureScores.columns = ['Specs','Score']  #naming the dataframe columns
+print(featureScores.nlargest(25,'Score'))  #print 25 best feature
+
+# Feature Selection
+model = ExtraTreesClassifier()
+model.fit(X,y)
+print(model.feature_importances_) #use inbuilt class feature_importances of tree based classifiers
+#plot graph of feature importances for better visualization
+feat_importances = pd.Series(model.feature_importances_, index=X.columns)
+feat_importances.nlargest(10).plot(kind='barh')
+plt.show()
+
