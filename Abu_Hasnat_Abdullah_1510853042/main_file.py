fed767962c76b2126e5732150b49969e051e15ff
abdullah <abdullah.2010bd@gmail.com>
2019-12-13 23:52:39 +0600

    Re-selecting feature for better accuracy

--- Abu_Hasnat_Abdullah_1510853042/main_file.py
+++ Abu_Hasnat_Abdullah_1510853042/main_file.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+from sklearn.ensemble import ExtraTreesClassifier
+import matplotlib.pyplot as plt
 from matplotlib import pyplot as plt
 
 df = pd.read_csv("Data/globalterrorismdb_0718dist.csv", encoding = "ISO-8859-1", low_memory = False)
