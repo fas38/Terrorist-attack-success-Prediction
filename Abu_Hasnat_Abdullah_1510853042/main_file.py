79bc4f781a41161a1a3b849f606530d80035b3c7
abdullah <abdullah.2010bd@gmail.com>
2019-12-16 00:20:07 +0600

    Applyed Linear regression

--- Abu_Hasnat_Abdullah_1510853042/main_file.py
+++ Abu_Hasnat_Abdullah_1510853042/main_file.py
@@ -10,19 +10,20 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import svm
 from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LinearRegression
 
 print("HEllo")
 data = pd.read_csv("globalterrorismdb_0718dist.csv", encoding = "ISO-8859-1", low_memory = False)
-X = data.iloc[:,0:133]  #independent columns
+x = data.iloc[:,0:133]  #independent columns
 y = data.iloc[:,-1]    #target column i.e price range
 
 bestfeatures = SelectKBest(score_func=chi2, k=25)
 
 #apply SelectKBest class to extract top 25 best features
 bestfeatures = SelectKBest(score_func=chi2, k=25)
-fit = bestfeatures.fit(X,y)
+fit = bestfeatures.fit(x,y)
 dfscores = pd.DataFrame(fit.scores_)
-dfcolumns = pd.DataFrame(X.columns)
+dfcolumns = pd.DataFrame(x.columns)
 #concat two dataframes for better visualization
 featureScores = pd.concat([dfcolumns,dfscores],axis=1)
 featureScores.columns = ['Specs','Score']  #naming the dataframe columns
@@ -30,16 +31,23 @@ print(featureScores.nlargest(25,'Score'))  #print 25 best feature
 
 # Feature Selection
 model = ExtraTreesClassifier()
-model.fit(X,y)
+model.fit(x,y)
 print(model.feature_importances_) #use inbuilt class feature_importances of tree based classifiers
 #plot graph of feature importances for better visualization
-feat_importances = pd.Series(model.feature_importances_, index=X.columns)
+feat_importances = pd.Series(model.feature_importances_, index=x.columns)
 feat_importances.nlargest(10).plot(kind='barh')
 plt.show()
 
 #K-Fold Cross Validation
 model = svm.SVC()
-accuracy = cross_val_score(model, X, y, scoring='accuracy', cv = 10)
+accuracy = cross_val_score(model, x, y, scoring='accuracy', cv = 10)
 print(accuracy)
 #get the mean of each fold
 print("Accuracy of Model with Cross Validation is:",accuracy.mean() * 100)
+
+#Linear regression
+predictor = LinearRegression(n_jobs=-1)
+predictor.fit(X=x, y=y)
+outcome = predictor.predict(X=X_TEST)
+coefficients = predictor.coef_
+print('Outcome : {}\nCoefficients : {}'.format(outcome, coefficients))
